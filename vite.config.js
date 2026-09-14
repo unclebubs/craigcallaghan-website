dc46f5fc0c643bd646922c4dff6ebc7ab1931641
Craig Callaghan <craig.callaghan@gmail.com>
2026-09-14 11:04:50 +0100

    Added new pages to build

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        cookiePolicy: resolve(__dirname, 'cookie-policy.html'),
+        privacyPolicy: resolve(__dirname, 'privacy-policy.html'),
+        terms: resolve(__dirname, 'terms.html'),
+      },
+    },
+  },
 })
